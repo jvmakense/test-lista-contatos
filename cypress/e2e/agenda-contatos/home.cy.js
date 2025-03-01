a544b0e4da57cd2cbfbef2b529de88a87635473e
joao vitor <joaomakenze@gmail.com>
2025-03-01 09:51:25 -0300

    iniciando o projeto cypress

---
+++ cypress/e2e/agenda-contatos/home.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Teste para a inclusão de um contato', () =>{
+    it('Deve renderizar 3 contatos', () => {
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+        cy.get('')
+    })
+})
