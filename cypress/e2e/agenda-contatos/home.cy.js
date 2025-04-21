5cb5ba078094db7662c7f43c2aedb592785c3871
joao vitor <joaomakenze@gmail.com>
2025-04-21 19:33:12 -0300

    corrigindo o  bug

--- cypress/e2e/agenda-contatos/home.cy.js
+++ cypress/e2e/agenda-contatos/home.cy.js
@@ -2,35 +2,40 @@
 
 describe('Testes para o contatos', () => {
     
-    beforeEach(()=>{
+    beforeEach(() => {
         cy.visit('https://agenda-contatos-cypress.vercel.app/')
     })
+
     it('Devera adicionar um novo contato', () => {
         cy.get('[type="text"]').type('Test')
         cy.get('[type="email"]').type('Test@gmail.com')
         cy.get('[type="tel"]').type('00 0000 0000')
         cy.get('.adicionar').click()
-    })  
+        cy.wait(10000)
+
+        cy.contains('Test').should('exist')
+    })
+
     it('Devera ativar modo de edição', () => {
         cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
         cy.get('[type="text"]').should('have.length.greaterThan', 0)
+        cy.wait(10000)
     })
+
     it('Devera editar o primeiro contato', () => {
         cy.get('.edit').first().click()
 
-        cy.get('[type="text"]').clear()
-        cy.get('[type="text"]').type('NovoTest')
-
-        cy.get('[type="email"]').clear()
-        cy.get('[type="email"]').type('NovoTest@gmail.com')
-
-        cy.get('[type="tel"]').clear()
-        cy.get('[type="tel"]').type('99 9999 9999')
-
+        cy.get('[type="text"]').clear().type('NovoTest')
+        cy.get('[type="email"]').clear().type('NovoTest@gmail.com')
+        cy.get('[type="tel"]').clear().type('99 9999 9999')
         cy.get('.alterar').click()
+        cy.wait(10000)
+        
+        cy.contains('NovoTest').should('exist')
     })
-    it('Devera excluir o ultimo contato', () => {
 
+    it('Devera excluir o ultimo contato', () => {
         cy.get('.delete').last().click()
+        cy.wait(5000)
     })
 })
